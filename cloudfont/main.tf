2651eeea6deef56cb8fb94319322f7abf5f14e8d
Aaretha <149208787+Aaretha@users.noreply.github.com>
2024-06-29 00:11:28 +0100

    Create main.tf

---
+++ cloudfont/main.tf
@@ -0,0 +1,42 @@
+resource "aws_cloudfront_distribution" "this" {
+  origin {
+    domain_name = var.s3_bucket_domain_name
+    origin_id   = var.origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.this.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  aliases = [var.domain_name]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = var.origin_id
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_All"
+
+  viewer_certificate {
+    acm_certificate_arn = var.acm_certificate_arn
+    ssl_support_method  = "sni-only"
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "this" {
+  comment = "Access identity for ${var.origin_id}"
+}
